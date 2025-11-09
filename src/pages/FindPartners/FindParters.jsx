5c45bd4501d2a80560672143dc1bb64b61d4135a
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-11-10 00:22:48 +0600

    all partner call form backend and set in find parten page

--- src/pages/FindPartners/FindParters.jsx
+++ src/pages/FindPartners/FindParters.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import PartnerCard from '../../components/PartnerCard';
+import React, { useEffect, useState } from "react";
+import PartnerCard from "../../components/PartnerCard";
 
 const FindParters = () => {
-    return (
-        <div>
-           . <div className="">
-            <h1 className='text-3xl text-primary text-center font-bold'>
-                Find Your Partner
-            </h1>
-           </div>
-           <div className="divider"></div>
-            <main className=' mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 items-center'>
-                <PartnerCard />
-                <PartnerCard />
-                <PartnerCard />
-                <PartnerCard />
-                <PartnerCard />
-                <PartnerCard />
-                <PartnerCard />
-                <PartnerCard />
-            </main>
-        </div>
-    );
+  const [partners, setParners] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/partners")
+      .then((res) => res.json())
+      .then((data) => {
+        setParners(data);
+      });
+  }, []);
+  console.log(partners);
+  return (
+    <div>
+      .{" "}
+      <div className="">
+        <h1 className="text-3xl text-primary text-center font-bold">
+          Find Your Partner
+        </h1>
+      </div>
+      <div className="divider"></div>
+      <main className=" mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 items-center">
+        {partners.map((partner) => (
+          <PartnerCard key={partner._id} Partner={partner} />
+        ))}
+      </main>
+    </div>
+  );
 };
 
 export default FindParters;
