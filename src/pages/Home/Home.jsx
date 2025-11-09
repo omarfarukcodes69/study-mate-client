672711134b1ff5735f4feda17926133b2d6b821d
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-11-09 13:52:55 +0600

    how it works section added in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroBanner from "./HeroBannar";
 import PartnerCard from "../../components/PartnerCard";
+import HowItWorks from "./HowItWorks";
 
 const Home = () => {
   return (
@@ -9,15 +10,24 @@ const Home = () => {
         <HeroBanner />
       </header>
       <main>
+        <section>
           <h1 className="text-3xl text-primary font-bold text-center my-10">
             Top Study Partners
           </h1>
-        <section className="w-9/12 mx-auto grid grid-cols-3 gap-2 items-center">
-          <PartnerCard/>
-          <PartnerCard/>
-          <PartnerCard/>
+          <div className="w-9/12 mx-auto grid grid-cols-3 gap-2 items-center">
+            <PartnerCard />
+            <PartnerCard />
+            <PartnerCard />
+          </div>
         </section>
-
+        <section className=" mx-auto bg-base-content rounded-3xl">
+          <h1 className="text-3xl w-fit text-primary bg-base-100 mx-auto p-2  rounded-b-2xl font-bold text-center my-10 shadow">
+            How It Works
+          </h1>
+          <div className="w-9/12 mx-auto mb-10 pb-10  ">
+            <HowItWorks />
+          </div>
+        </section>
       </main>
     </div>
   );
---
+++ src/pages/Home/HowItWorks.jsx
@@ -0,0 +1,25 @@
+import steps from "../../../public/steps.json";
+
+const HowItWorks = () => {
+  return (
+    <div className="py-1 grid grid-cols-3 gap-8 items-center ">
+      {steps.map(({ id, title, description, iconUrl, iconColor }) => (
+        <div
+          key={id}
+          className=" p-6 border-2 border-primary rounded-lg shadow-xl hover:shadow-lg transition hover:bg-primary-content   hover:scale-115"
+        >
+          <img
+            src={iconUrl}
+            alt={`${title} icon`}
+            className="w-10 h-10 mb-3"
+            style={{ filter: `drop-shadow(0 0 2px ${iconColor})` }}
+          />
+          <h3 className="text-xl font-semibold mb-3 text-base-200">{title}</h3>
+          <p className="text-base-300">{description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HowItWorks;
