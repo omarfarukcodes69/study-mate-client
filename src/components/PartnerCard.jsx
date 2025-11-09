c18f88ec63903500be6f0c61862ef04529ab42db
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-11-09 13:13:03 +0600

    partner card design added in home page

---
+++ src/components/PartnerCard.jsx
@@ -0,0 +1,25 @@
+// TopPartnerCard.jsx
+import React from "react";
+import { FaUser } from "react-icons/fa";
+
+const PartnerCard = () => {
+  // { name:'omar faruk', subject:'math', rating:'5', imgUrl:""}
+  return (
+    <div className="w-xs mb-10 bg-white rounded-lg shadow-md p-6 flex flex-col items-center hover:scale-115 hover:bg-accent  transition-transform cursor-pointer">
+      <figure >
+        <FaUser className="w-24 h-24 rounded-full object-cover mb-4 border-4   border-primary"/>
+      </figure>
+
+      <h3 className="text-xl font-semibold text-gray-800 mb-1">{name}</h3>
+      <p className=" mb-2">
+        <span className="font-semibold">Subject:</span> subject
+      </p>
+      <p className="text-yellow-400 font-bold">Rating:"5"</p>
+      <button className="mt-4 btn btn-primary hover:btn-primary text-white rounded-md font-semibold transition">
+        View Profile
+      </button>
+    </div>
+  );
+};
+
+export default PartnerCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HeroBanner from "./HeroBannar";
+import PartnerCard from "../../components/PartnerCard";
 
 const Home = () => {
   return (
@@ -8,11 +9,15 @@ const Home = () => {
         <HeroBanner />
       </header>
       <main>
-        <section>
           <h1 className="text-3xl text-primary font-bold text-center my-10">
             Top Study Partners
           </h1>
+        <section className="w-9/12 mx-auto grid grid-cols-3 gap-2 items-center">
+          <PartnerCard/>
+          <PartnerCard/>
+          <PartnerCard/>
         </section>
+
       </main>
     </div>
   );
