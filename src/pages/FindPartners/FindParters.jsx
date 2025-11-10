57345f04f0fe2605b12d6f27976fbd5313136fe4
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-11-10 15:03:07 +0600

    search feature added

--- src/pages/FindPartners/FindParters.jsx
+++ src/pages/FindPartners/FindParters.jsx
@@ -4,8 +4,7 @@ import { toast } from "react-toastify";
 
 const FindParters = () => {
   const [partners, setParners] = useState([]);
-  const [search, setSearch] = useState("");
-  const [sort, setSort] = useState("default");
+  const [searchValue, setSearchValue] = useState("");
   useEffect(() => {
     fetch("http://localhost:3000/partners")
       .then((res) => res.json())
@@ -13,15 +12,16 @@ const FindParters = () => {
         setParners(data);
       });
   }, []);
-  console.log(partners);
+  // console.log(partners);
+  console.log(searchValue);
 
-  // === Handle Search ===
-
-  const handleSearch = (e) => {
-    setSearch(e.target.value);
+  // ===handleSearch===
+  const handleSearch = () => {
+     const searchPartner = partners.filter((partner) =>
+      partner.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setParners(searchPartner)
   };
-
-
   return (
     <div>
       <div className="">
@@ -34,25 +34,25 @@ const FindParters = () => {
       <div className="flex flex-col md:flex-row justify-between items-center mb-10">
         <div className="w-full space-x-4 mb-3 md:mb-0">
           <label className="font-semibold text-accent">Sort by:</label>
-          <select
-            onChange={(e) => setSort(e.target.value)}
-            className="select select-bordered w-fit  "
-          >
+          <select className="select select-bordered w-fit  ">
             <option value="default">Default</option>
             <option value="name">Name</option>
             <option value="experience">Experience Level</option>
           </select>
         </div>
-
+        {/* ==== search section ==== */}
         <div className="flex items-center gap-3">
           <input
             type="text"
             placeholder="Search by name..."
-            value={search}
-            onChange={handleSearch}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
             className="input input-bordered w-full md:w-64"
           />
-          <button className="btn btn-primary text-white">Search</button>
+          <button onClick={handleSearch} className="btn btn-primary text-white">
+            Search
+          </button>
         </div>
       </div>{" "}
       <main className=" mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 items-center">
